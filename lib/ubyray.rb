678a15cd2962a0843354091c38f893b435ca1f72
Brian Kelly <polymonic@gmail.com>
2012-08-15 22:04:55 -0400

    Fixed check for Ruby < 1.9 and added same check to starts_with_capital?

--- lib/ubyray.rb
+++ lib/ubyray.rb
@@ -2,9 +2,10 @@ require "ubyray/version"
 
 module Ubyray
   def self.translate(word)
-    letter = word[0].downcase
     if RUBY_VERSION < "1.9"
       letter = word[0].chr.downcase
+    else
+      letter = word[0].downcase
     end
 
     rest = word[1..-1]
@@ -13,7 +14,14 @@ module Ubyray
   end
   
   def self.starts_with_capital?(word)
-    word[0].ord < 97
+    if RUBY_VERSION < "1.9"
+      ord = word[0]
+    else
+      ord = word[0].ord
+    end
+
+    ord < 97
   end
+
 end
 
