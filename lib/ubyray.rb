77c629b788964a04b2f4f0bd097e6976ef798f3d
Brian Kelly <polymonic@gmail.com>
2012-08-16 23:14:13 -0400

    Can now translate words starting with vowels

--- lib/ubyray.rb
+++ lib/ubyray.rb
@@ -2,17 +2,33 @@ require "ubyray/version"
 
 module Ubyray
   def self.translate(word)
+    letter = self.first_letter(word)
+
+    if vowelized?(word)
+      return "#{word}way"
+    else
+      rest = word[1..-1]
+      rest.capitalize! if self.capitalized?(word)
+      return "#{rest}#{letter}ay"
+    end
+  end
+
+  def self.first_letter(word)
     if RUBY_VERSION < "1.9"
       letter = word[0].chr.downcase
     else
       letter = word[0].downcase
     end
 
-    rest = word[1..-1]
-    rest.capitalize! if self.capitalized?(word)
-    return "#{rest}#{letter}ay"
+    letter
   end
-  
+
+  def self.vowelized?(word)
+    letter = self.first_letter(word)
+
+    ["a","e","i","o","u"].include?(letter.downcase)
+  end
+
   def self.capitalized?(word)
     if RUBY_VERSION < "1.9"
       ord = word[0]
--- spec/lib/ubyray_spec.rb
+++ spec/lib/ubyray_spec.rb
@@ -8,7 +8,19 @@ describe Ubyray do
   it "should translate words starting with upper case consanants" do
     Ubyray.translate("Ruby").should eq "Ubyray"
   end
-
+  
+  it "should translate words starting with a lower case vowel" do
+    Ubyray.translate("apple").should eq "appleway"
+  end
+  
+  it "should translate words starting with a upper case vowel" do
+    Ubyray.translate("Apple").should eq "Appleway"
+  end
+  
+  it "should detect if a word started with a vowel" do
+    Ubyray.vowelized?("apple").should eq true
+  end
+  
   it "should detect if word start with a capital" do
     Ubyray.capitalized?("Foo").should eq true
   end
