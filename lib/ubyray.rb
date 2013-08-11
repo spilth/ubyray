a6b5b63a4253052d503576c7f55b7351a203e28e
Brian Kelly <polymonic@gmail.com>
2013-08-10 22:21:17 -0400

    Removed self. from Ubyray class where not necessary and removed double quotes where not necessary

--- lib/ubyray.rb
+++ lib/ubyray.rb
@@ -1,4 +1,4 @@
-require "ubyray/version"
+require 'ubyray/version'
 
 module Ubyray
   def self.translate(sentence)
@@ -6,35 +6,32 @@ module Ubyray
     piglatin_words = []
 
     words.each do |word|
-      piglatin_words << self.translate_word(word)
+      piglatin_words << translate_word(word)
     end
 
     piglatin_words.join(' ')
   end
 
   def self.translate_word(word)
-    letter = self.first_letter(word)
-    piglatin = ""
     if vowelized?(word)
       piglatin = "#{word}way"
     else
-      split_index = self.first_vowel_index(word)
+      split_index = first_vowel_index(word)
       rest = word[0..split_index-1].downcase
       start = word[split_index..-1]
-      start.capitalize! if self.capitalized?(word)
+      start.capitalize! if capitalized?(word)
       piglatin = "#{start}#{rest}ay"
     end
-    piglatin
   end
 
   def self.first_letter(word)
-    RUBY_VERSION < "1.9" ? word[0].chr.downcase : word[0].downcase
+    RUBY_VERSION < '1.9' ? word[0].chr.downcase : word[0].downcase
   end
 
   def self.first_vowel_index(word)
     i = 0
     word.split(//).each do |letter|
-      if self.vowel?(letter)
+      if vowel?(letter)
         break
       end
       i += 1
@@ -43,27 +40,25 @@ module Ubyray
   end
 
   def self.letter?(character)
-    ("a".."z").include?(character.downcase)
+    ('a'..'z').include?(character.downcase)
   end
 
   def self.vowel?(letter)
-    ["a","e","i","o","u","y"].include?(letter.downcase)
+    %w{a e i o u y}.include?(letter.downcase)
   end
 
   def self.vowelized?(word)
-    letter = self.first_letter(word)
-
-    self.vowel?(letter)
+    vowel? first_letter(word)
   end
 
   def self.capitalized?(word)
-    ord = RUBY_VERSION < "1.9" ? word[0] : word[0].ord
+    ord = RUBY_VERSION < '1.9' ? word[0] : word[0].ord
     ord < 97
   end
 
   def self.to_a(sentence)
     return [""] if sentence == ""
-    
+
     pieces = []
   end
 
