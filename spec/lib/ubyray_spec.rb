bee30bc43e578bbc4db4b1fec5d8903cbcd0f9a1
Brian Kelly <polymonic@gmail.com>
2013-08-10 21:45:59 -0400

    Using new expect(x) syntax for specs and switched to using describe blocks with method names

--- spec/lib/ubyray_spec.rb
+++ spec/lib/ubyray_spec.rb
@@ -1,87 +1,95 @@
 require 'spec_helper'
 
 describe Ubyray do
-  context "word translation" do
-    context "words starting with constanants" do
-      it "should translate words starting with a single lower case consanant" do
-        Ubyray.translate("ruby").should eq "ubyray"
+  describe ".translate" do
+    context "a single word" do
+      context "starting with a single constanant" do
+        it "maintains the first letter as lowercase" do
+          expect(Ubyray.translate 'ruby').to eq 'ubyray'
+        end
+
+        it "maintains the first letter as uppercase" do
+          expect(Ubyray.translate 'Ruby').to eq 'Ubyray'
+        end
       end
 
-      it "should translate words starting with a single upper case consanant" do
-        Ubyray.translate("Ruby").should eq "Ubyray"
+      context "starting with multiple consanants" do
+        it "maintains the first letter as lowercase" do
+          expect(Ubyray.translate 'pragmatic').to eq 'agmaticpray'
+        end
+
+        it "maintains the first letter as uppercase" do
+          expect(Ubyray.translate 'Pragmatic').to eq 'Agmaticpray'
+        end
       end
-      
-      it "should translate words starting with multiple lower case consanants" do
-        Ubyray.translate("pragmatic").should eq "agmaticpray"
-      end
-      
-      it "should translate words starting with multiple upper case consanants" do
-        Ubyray.translate("Pragmatic").should eq "Agmaticpray"
+
+      context "starting with a vowel" do
+        it "adds 'way' to a lowercase word" do
+          expect(Ubyray.translate 'apple').to eq 'appleway'
+        end
+
+        it "adds 'way' to a capitalized word" do
+          expect(Ubyray.translate 'Apple').to eq 'Appleway'
+        end
       end
     end
-   
-    context "words starting with vowels" do
-      it "should translate words starting with a lower case vowel" do
-        Ubyray.translate("apple").should eq "appleway"
+
+    context "a sentence" do
+      it "should translate sentences" do
+        Ubyray.translate("Hello world").should eq "Ellohay orldway"
       end
-      
-      it "should translate words starting with a upper case vowel" do
-        Ubyray.translate("Apple").should eq "Appleway"
+
+      it "should translate sentences with punctuation" do
+        pending("Not working yet")
+        #Ubyray.translate('This is a sentence.').should eq 'Isthay isway away entencesay.'
       end
     end
   end
 
-  context "sentence translations" do
-    it "should translate sentences" do
-      Ubyray.translate("Hello world").should eq "Ellohay orldway"
+  describe ".letter?" do
+    it "determines if a character is a letter or not" do
+      expect(Ubyray.letter? 'a').to eq true
+      expect(Ubyray.letter? 'Z').to eq true
+      expect(Ubyray.letter? '-').to eq false
+      expect(Ubyray.letter? '!').to eq false
+      expect(Ubyray.letter? '3').to eq false
+      expect(Ubyray.letter? ' ').to eq false
+    end
+  end
+
+  describe ".first_vowerl_index" do
+    it "finds the index of the first vowel in a word" do
+      expect(Ubyray.first_vowel_index 'apple').to eq 0
+      expect(Ubyray.first_vowel_index 'ruby').to eq 1
+      expect(Ubyray.first_vowel_index 'Pragmatic').to eq 2
+      expect(Ubyray.first_vowel_index 'pry').to eq 2
+    end
+  end
+
+  describe ".vowelized?" do
+    it "determines if a word starts a vowel" do
+      expect(Ubyray.vowelized? 'Apple').to eq true
+      expect(Ubyray.vowelized? 'apple').to eq true
+      expect(Ubyray.vowelized? 'Microsoft').to eq false
+      expect(Ubyray.vowelized? 'microsoft').to eq false
+    end
+  end
+
+  describe ".capitalized?" do
+    it "determines if a word starts with a capital letter" do
+      expect(Ubyray.capitalized? 'Foo').to eq true
+      expect(Ubyray.capitalized? 'foo').to eq false
+    end
+  end
+
+  describe ".to_a" do
+    it "should return an array with an empty string for an empty sentence" do
+      expect(Ubyray.to_a '').to eq ['']
     end
 
-    it "should translate sentences with punctuation" do
+    it "turns a sentence into an array of pieces" do
       pending("Not working yet")
-      #Ubyray.translate('This is a sentence.').should eq 'Isthay isway away entencesay.'
-    end
-  end
-
-  context "character helper methods" do
-    it "should know if a character is a letter or not" do
-      Ubyray.letter?("a").should eq true
-      Ubyray.letter?("Z").should eq true
-      Ubyray.letter?("-").should eq false
-      Ubyray.letter?("!").should eq false
-      Ubyray.letter?("3").should eq false
-      Ubyray.letter?(" ").should eq false
-    end
-  end
-
-  context "word helper methods" do
-    it "should find the index of the first vowel in a word" do
-      Ubyray.first_vowel_index("apple").should eq 0
-      Ubyray.first_vowel_index("ruby").should eq 1
-      Ubyray.first_vowel_index("Pragmatic").should eq 2
-      Ubyray.first_vowel_index("pry").should eq 2
-    end
-    
-    it "should detect if a word started with a vowel" do
-      Ubyray.vowelized?("apple").should eq true
-    end
-    
-    it "should detect if word start with a capital" do
-      Ubyray.capitalized?("Foo").should eq true
-    end
-
-    it "should detect if word doesn't start with a capital" do
-      Ubyray.capitalized?("bar").should eq false
-    end
-  end
-
-  context "sentence helper methods" do
-    it 'should return an array with an empty string for an empty sentence' do
-      Ubyray.to_a("").should eq [""]
-    end
-
-    it 'should turn a sentence into an array of pieces' do
-      pending("Not working yet")
-      Ubyray.to_a("First third fifth").should eq ['First', ' ', 'third', ' ', 'fifth']
+      #Ubyray.to_a("First third fifth").should eq ['First', ' ', 'third', ' ', 'fifth']
     end
   end
 end
